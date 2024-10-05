82d5a76c1981f5b58237a14e2c894aee6f8894c4
Jorge Ramirez <jorge_ramirez13@usmp.pe>
2024-10-04 19:39:01 -0500

    Creación del proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Examen_Parcial_20242.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
